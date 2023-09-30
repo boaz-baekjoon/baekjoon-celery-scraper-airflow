5d67651e4c8032a6aa0de081ef399bdafe4ad2c0
HaJunYoo <hajuny129@gmail.com>
2023-09-30 13:00:26 +0900

    fastapi port change

--- scraper_server/server.py
+++ scraper_server/server.py
@@ -1,4 +1,4 @@
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run("app:app", host="0.0.0.0", port=27000, log_level="info", reload=True)
+    uvicorn.run("app:app", host="0.0.0.0", port=27500, log_level="info", reload=True)
