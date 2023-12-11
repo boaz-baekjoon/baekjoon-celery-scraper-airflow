f772aa294ac43d56993eabf0b2e301225d1e23c2
유하준 <yuhajun@yuhajun-ui-MacBookPro.local>
2023-12-12 00:15:13 +0900

    feat: scrapy user_detail

--- scrapy/baekjoon_scraper/baekjoon_scraper/items.py
+++ scrapy/baekjoon_scraper/baekjoon_scraper/items.py
@@ -39,3 +39,23 @@ class WorkbookItem(scrapy.Item):
     def __init__(self, *args, **kwargs):
         super(WorkbookItem, self).__init__(*args, **kwargs)
         self._values = OrderedDict()
+
+
+class ProblemDetailItem(scrapy.Item):
+    problem_id = scrapy.Field()
+    problem_title = scrapy.Field()
+    problem_lang = scrapy.Field()
+    tag_display_lang = scrapy.Field()
+    tag_name = scrapy.Field()
+    problem_titles_isOriginal = scrapy.Field()
+    problem_isSolvable = scrapy.Field()
+    problem_isPartial = scrapy.Field()
+    problem_answer_num = scrapy.Field()
+    problem_level = scrapy.Field()
+    problem_votedUserCount = scrapy.Field()
+    problem_sprout = scrapy.Field()
+    problem_givesNoRating = scrapy.Field()
+    problem_isLevelLocked = scrapy.Field()
+    problem_averageTries = scrapy.Field()
+    problem_official = scrapy.Field()
+    tag_key = scrapy.Field()
--- scrapy/baekjoon_scraper/baekjoon_scraper/pipelines.py
+++ scrapy/baekjoon_scraper/baekjoon_scraper/pipelines.py
@@ -7,7 +7,7 @@
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
 from collections import defaultdict
-from sqlalchemy import create_engine, Table, MetaData
+from sqlalchemy import create_engine, Table, MetaData, text
 from sqlalchemy.orm import sessionmaker
 import polars as pl
 from baekjoon_scraper.config import config
@@ -37,22 +37,36 @@ class RDSFullRefreshPipeline:
                 sorted_items = sorted(items, key=lambda i: i['problem_id'])
                 df = pd.DataFrame(sorted_items)
                 df.set_index(['problem_id'], inplace=True)
+
             elif item_type == 'UserItem':
                 table_name = 'users'
                 sorted_items = sorted(items, key=lambda i: i['user_rank'])
                 df = pd.DataFrame(sorted_items)
                 df.set_index(['user_id'], inplace=True)
+
             elif item_type == 'WorkbookItem':
                 table_name = 'workbooks'
                 sorted_items = sorted(items, key=lambda i: i['workbook_rank'])
                 df = pd.DataFrame(sorted_items)
                 df.set_index(['workbook_id', 'problem_id'], inplace=True)
+
+            elif item_type == 'ProblemDetailItem':
+                table_name = 'problem_details'
+                sorted_items = sorted(items, key=lambda i: i['problem_id'])
+                df = pd.DataFrame(sorted_items)
+                df.set_index(['problem_id', 'problem_level', 'tag_key'], inplace=True)
+
             else:
                 sorted_items = items
 
             try:
                 df.to_sql(table_name, con=self.engine, if_exists='replace', index=True)
                 logging.warning(f'close_spider - pipeline - {table_name} table created')
+
+                if df is not None and table_name is not None:
+                    self.set_primary_key(table_name)
+                    logging.warning(f'close_spider - pipeline - {table_name} pk set')
+
             except Exception as e:
                 logging.warning(f'close_spider - pipeline - {table_name} table creation failed')
                 logging.warning(e)
@@ -76,4 +90,23 @@ class RDSFullRefreshPipeline:
         engine = create_engine(f'postgresql+psycopg2://{username}:{password}@{host}:{port}/{database}')
 
         return engine
+
+    def set_primary_key(self, table_name):
+        if table_name == 'problems':
+            pk_column = 'problem_id'
+        elif table_name == 'users':
+            pk_column = 'user_id'
+        elif table_name == 'workbooks':
+            pk_column = 'workbook_id, problem_id'
+        elif table_name == 'problem_details':
+            pk_column = 'problem_id, problem_level, tag_key'
+        else:
+            return  # 기본 키 설정할 필요 없는 경우
+
+        try:
+            with self.engine.connect() as conn:
+                conn.execute(text(f"ALTER TABLE {table_name} ADD PRIMARY KEY ({pk_column})"))
+                logging.warning(f'{table_name} table - Primary key set on {pk_column}')
+        except Exception as e:
+            logging.warning(f'Failed to set primary key on {table_name}: {e}')
 
